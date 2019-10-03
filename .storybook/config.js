86eca3487f2661815c7bbcb8e8095cfba4495883
dewilde86 <>
2019-10-03 14:17:51 +0200

    setup storybooks

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,21 @@
 import { configure } from '@storybook/react';
 
-// automatically import all files ending in *.stories.js
-configure(require.context('../src/stories', true, /\.stories\.js$/), module);
+// automatically import all files ending in *.stories.js|mdx
+configure(
+  [
+    require.context('../src', false, /Intro\.stories\.mdx/),
+    require.context('../src', true, /\.stories\.(js|mdx)$/),
+  ],
+  module
+ );
+
+module.exports = function({ config }) {
+    config.module.rules.push({
+      test: /\.stories\.jsx?$/,
+      loaders: [require.resolve('@storybook/addon-storysource/loader')],
+      enforce: 'pre',
+    });
+   
+    return config;
+};
+
